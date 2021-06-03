da0dfd871683c340ddb281bee1b8144bc2562a56
Sine-mora <batlefield.desert.combat@gmail.com>
2021-06-03 23:18:16 +0300

    Removed dirs folder in RenderTest

--- sandbox/RenderTest/dirs/Renderer/libs/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Desert Combat/source/repos/DungeonEscape/libs/k9/examples/ImGui_Example/Renderer/libs/glm/glm
-# Build directory: C:/Users/Desert Combat/source/repos/DungeonEscape/sandbox/RenderTest/dirs/Renderer/libs/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
