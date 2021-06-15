435caa7a156cc7adf80e843ce45a31219217055d
Sine-mora <batlefield.desert.combat@gmail.com>
2021-06-15 21:14:34 +0300

    cleaned from libs sandbox

---
+++ sandbox/sdlandCmake/dirs/glm/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Desert Combat/source/repos/DungeonEscape/libs/k9/examples/ImGui_Example/Renderer/libs/glm
+# Build directory: C:/Users/Desert Combat/source/repos/DungeonEscape/sandbox/sdlandCmake/dirs/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
--- sandbox/sdlandCmake/src/main.cpp
+++ sandbox/sdlandCmake/src/main.cpp
@@ -1,7 +1,4 @@
-#include <SDL.h>
-#include <SDL_ttf.h>
 #include <SDL_mixer.h>
-#include <SDL_image.h>
 #include <sol.hpp>
 #include <iostream>
 #include <string>
